d41a2fdc148c36c7167c53853455ae1ec7799575
PierreGUI <pierre.vdev@gmail.com>
2016-06-19 00:05:45 +0200

    Disable activerecord config to fix build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # config.active_record.dump_schema_after_migration = false
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
 end
